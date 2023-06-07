eca66db083aab19c3c4b640539bf86a3ed1a06df
fiendrsy <fiendrsy@PC.local>
2023-06-07 11:04:57 +0300

    added functions read and save from storage

---
+++ popup/scripts/storage.js
@@ -0,0 +1,16 @@
+export async function readFromStorage(key) {
+	try {
+		let data = await browser.storage.local.get(key);
+		return data[key];
+	} catch (e) {
+		throw new Error("get from storage error", e);
+	}
+}
+
+export async function saveToStorage(data) {
+	try {
+		await browser.storage.local.set(data);
+	} catch (e) {
+		throw new Error("save to storage error", e);
+	}
+}
