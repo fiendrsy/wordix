28094d0999f6838c5dd11e9dc5939065b657fe4d
fiendrsy <fiendrsy@PC.local>
2023-06-07 12:30:34 +0300

    added connection to local storage, saving selected words, code refactoring

--- popup/scripts/main.js
+++ popup/scripts/main.js
@@ -2,11 +2,12 @@
 
 import getActiveTab from "../../helpers/get-active-tab.js";
 import tabsSendMessage from "../../helpers/tabs-send-message.js";
+import { readFromStorage, saveToStorage } from "./storage.js";
 
 let activeTab;
 let minRepeats;
 let searchWord;
-let olElement = document.querySelector("#parsed-words-list");
+let olElement = document.getElementById("parsed-words-list");
 
 (async () => {
 	let data = await getActiveTab();
@@ -21,14 +22,16 @@ function insertInDocumentContent([word, count], index) {
 	checkboxElement.id = `parsed-word__${index}`;
 	labelElement.textContent = `${word} -> ${count} times`;
 	labelElement.htmlFor = `${checkboxElement.id}`;
+	liElement.setAttribute("id", `${index}`);
 	liElement.append(checkboxElement);
 	liElement.append(labelElement);
 	olElement.append(liElement);
+	checkboxElement.addEventListener("change", selectedWordHandler);
 }
 
 async function parseHandler(e) {
-	minRepeats = document.querySelector("#minimum-repeats").value.trim();
-	searchWord = document.querySelector("#search-by-word").value.trim();
+	minRepeats = document.getElementById("minimum-repeats").value.trim();
+	searchWord = document.getElementById("search-by-word").value.trim();
 	olElement.textContent = "";
 	let tabId = activeTab[0].id;
 	let message = { minRepeats, searchWord };
@@ -38,12 +41,35 @@ async function parseHandler(e) {
 	parsedWords.forEach(insertInDocumentContent);
 }
 
+async function selectedWordHandler(e) {
+	const date = new Date();
+	let word = e.target.parentNode.innerText.split(" ")[0];
+	let numRepetitions = parseInt(e.target.parentNode.innerText.split(" ")[2]);
+	let liElementId = e.target.parentNode.id;
+	let checkboxElement = e.target;
+	let liElement = document.getElementById(liElementId);
+	let description = {
+		addedDate: date,
+		context: [],
+		numRepetitions,
+	};
+	let data = await readFromStorage(word);
+	if (!data) {
+		await saveToStorage({ [word]: description });
+		liElement.remove();
+	} else {
+		checkboxElement.removeAttribute("type");
+		liElement.textContent += " (ALREDY STORED)";
+		liElement.classList.add("alredy-stored");
+	}
+}
+
 document
-	.querySelector("#parse-button")
+	.getElementById("parse-button")
 	.addEventListener("click", parseHandler);
 document
-	.querySelector("#search-by-word")
+	.getElementById("search-by-word")
 	.addEventListener("input", parseHandler);
 document
-	.querySelector("#minimum-repeats")
+	.getElementById("minimum-repeats")
 	.addEventListener("input", parseHandler);
