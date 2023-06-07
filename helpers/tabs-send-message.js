07dbc102bc2875e4a2904a790d81f4c5b5a62895
fiendrsy <fiendrsy@PC.local>
2023-06-07 06:40:14 +0300

    added file

---
+++ helpers/tabs-send-message.js
@@ -0,0 +1,8 @@
+export default function tabsSendMessage(tabId, message) {
+	try {
+		let response = browser.tabs.sendMessage(tabId, message);
+		return response;
+	} catch (e) {
+		throw new Error("tabs send message error", e);
+	}
+}
