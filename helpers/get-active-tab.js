7628efa71d031cf24bdb6459e50789e3c756efbe
fiendrsy <fiendrsy@PC.local>
2023-06-07 06:37:14 +0300

    added try&catch

--- helpers/get-active-tab.js
+++ helpers/get-active-tab.js
@@ -1,5 +1,9 @@
 export default async function getActiveTab() {
-	let queryOption = { active: true, currentWindow: true };
-	let data = await browser.tabs.query(queryOption);
-	return data;
+	try {
+		let queryOption = { active: true, currentWindow: true };
+		let data = await browser.tabs.query(queryOption);
+		return data;
+	} catch (e) {
+		throw new Error("get active tab error", e);
+	}
 };
