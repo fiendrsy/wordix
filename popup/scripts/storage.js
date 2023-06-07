2d112a3cb7c1a0a810e01be7a84277b446fa4cbf
fiendrsy <fiendrsy@PC.local>
2023-06-07 13:21:51 +0300

    code refactoring

--- popup/scripts/storage.js
+++ popup/scripts/storage.js
@@ -1,9 +1,9 @@
 export async function readFromStorage(key) {
 	try {
-		let data = await browser.storage.local.get(key);
+		let data = await browser.storage.local.get([key]);
 		return data[key];
 	} catch (e) {
-		throw new Error("get from storage error", e);
+		throw new Error("read from storage error", e);
 	}
 }
 
