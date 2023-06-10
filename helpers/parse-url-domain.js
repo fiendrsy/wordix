ceba500abc004d6a27e5f9ca36086caa807bb93e
fiendrsy <fiendrsy@PC.local>
2023-06-10 03:59:23 +0300

    added possibility parse sub-domain

--- helpers/parse-url-domain.js
+++ helpers/parse-url-domain.js
@@ -1,6 +1,25 @@
-export default function parseUrlDomain(url) {
-	const pattern =
-		/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/gim;
-	let urlWitoutPath = url.match(pattern);
-	return urlWitoutPath[0].replace("https://", "");
+export default function parseUrlDomain(url, level) {
+	const pattern = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)/gim;
+	let urlWithoutPath = url.match(pattern);
+	let urlWithoutSchema = urlWithoutPath[0].replace("https://", "");
+	if (level) {
+		let domains = urlWithoutSchema.split(".");
+		if (domains.length > 2) {
+			switch (level) {
+				case "top":
+					return domains[2];
+				case "second":
+					return domains[1];
+				case "third":
+					return domains[0];
+			}
+		}
+		switch (level) {
+			case "top":
+				return domains[1];
+			case "second":
+				return domains[0];
+		}
+	}
+	return urlWithoutSchema;
 }
