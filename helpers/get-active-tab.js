a935e468c06944e065ebfdbd97c9e180fd23d011
fiendrsy <fiendrsy@PC.local>
2023-06-10 03:57:30 +0300

    code refactoring

--- helpers/get-active-tab.js
+++ helpers/get-active-tab.js
@@ -1,7 +1,7 @@
 export default async function getActiveTab() {
 	try {
 		let queryOption = { active: true, currentWindow: true };
-		let data = await browser.tabs.query(queryOption);
+		let [data] = await browser.tabs.query(queryOption);
 		return data;
 	} catch (e) {
 		throw new Error("get active tab error", e);
