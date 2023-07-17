9151273e3333aca7641d1faaea6868e47cd6970c
fiendrsy <fiendrsy@PC.local>
2023-07-17 16:27:59 +0300

    added the ability to use inputs

--- popup/scripts/session.js
+++ popup/scripts/session.js
@@ -1,23 +1,33 @@
 "use strict";
 
 import readAllFromStorage from "../../storage/read-all-from-storage.js";
+import readFromStorage from "../../storage/read-from-storage.js";
 
 let olElement = document.getElementById("session-list");
+let searchWord;
+let minRepeats;
+let limitWords;
+let byDomain;
 
 async function uploadHandler() {
 	olElement.textContent = "";
-	let wordFrequency = await getAllWordFrequencyFromStorage();
-	wordFrequency.forEach(insertContent);
+	const wordFrequency = await getAllWordFrequencyFromStorage();
+	const dataInputs = collectDataInputs(wordFrequency.length);
+	processing(dataInputs, wordFrequency);
 }
 
 async function getAllWordFrequencyFromStorage() {
+	byDomain = document.getElementById("session-by-site").value.trim();
+	if (byDomain.length > 1) {
+		const { wordFrequency } = await readFromStorage(byDomain);
+		return wordFrequency;
+	}
 	const storageData = await readAllFromStorage();
 	const wordFrequency = [];
 	for (let domain in storageData) {
 		wordFrequency.push(...storageData[domain].wordFrequency);
 	}
 	const concateWordFrequency = concatWordFrequency(wordFrequency);
-	concateWordFrequency.sort((a, b) => b[1] - a[1]);
 	return concateWordFrequency;
 }
 
@@ -39,6 +49,35 @@ function insertContent([word, frequency]) {
 	olElement.append(liElement);
 }
 
-async function isSearchWord() {}
+async function isSearchWord() {
+	olElement.textContent = "";
+	const wordFrequency = await getAllWordFrequencyFromStorage();
+	const dataInputs = collectDataInputs(wordFrequency.length);
+	const result = wordFrequency.filter(([word]) => word === dataInputs.searchWord);
+	processing(dataInputs, result);
+}
+
+function collectDataInputs(wordFrequencyLength) {
+	minRepeats = document.getElementById("session-min-repeats").value.trim();
+	limitWords = document.getElementById("session-limit-words").value.trim();
+	searchWord = document.getElementById("session-search-word").value.trim();
+	let min = minRepeats ? +minRepeats : 1;
+	let limit = limitWords ? +limitWords : wordFrequencyLength;
+	return {
+		min,
+		limit,
+		searchWord,
+	};
+}
+
+function processing({ limit, min } = dataInputs, wordFrequency) {
+	const result = wordFrequency.filter(([_, frequency]) => frequency >= min);
+	result.sort((a, b) => b[1] - a[1]);
+	let len = result.length < limit ? result.length : limit;
+	for (let i = 0; i < len; ++i) {
+		insertContent(result[i]);
+	}
+}
 
 document.querySelector(".upload-button").addEventListener("click", uploadHandler);
+document.getElementById("session-search-word").addEventListener("input", isSearchWord);
