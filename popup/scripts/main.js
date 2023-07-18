19aa6825baae06bfaf739e4986207ccc973d04b8
fiendrsy <fiendrsy@PC.local>
2023-07-19 00:06:42 +0300

    added ability select words, refactor

--- popup/scripts/main.js
+++ popup/scripts/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import getActiveTab from "../../helpers/get-active-tab.js";
+import parseUrlDomain from "../../helpers/parse-url-domain.js";
 import tabsSendMessage from "../../helpers/tabs-send-message.js";
 import readFromStorage from "../../storage/read-from-storage.js";
 import saveToStorage from "../../storage/save-to-storage.js";
@@ -8,21 +9,27 @@ import saveToStorage from "../../storage/save-to-storage.js";
 let activeTab;
 let minRepeats;
 let searchWord;
-let olElement = document.getElementById("parsed-words-list");
+const olElement = document.querySelector(".parsed-words__list");
+const currSite = document.querySelector(".current-site-address");
 
 (async () => {
-	let data = await getActiveTab();
+	const data = await getActiveTab();
 	activeTab = data;
+	currSite.textContent = parseUrlDomain(activeTab.url);
 })();
 
 function insertInDocumentContent([word, count], index) {
-	let liElement = document.createElement("li");
-	let labelElement = document.createElement("label");
-	let checkboxElement = document.createElement("input");
+	const aElement = document.createElement("a");
+	const liElement = document.createElement("li");
+	const labelElement = document.createElement("label");
+	const checkboxElement = document.createElement("input");
+	aElement.setAttribute("href", `https://context.reverso.net/translation/english-russian/${word}`);
+	aElement.classList.add("context-link");
 	checkboxElement.setAttribute("type", "checkbox");
 	checkboxElement.id = `parsed-word__${index}`;
-	labelElement.textContent = `${word} -> ${count} times`;
 	labelElement.htmlFor = `${checkboxElement.id}`;
+	aElement.textContent = `${word} -> ${count} times`;
+	labelElement.append(aElement);
 	liElement.setAttribute("id", `${index}`);
 	liElement.append(checkboxElement);
 	liElement.append(labelElement);
@@ -31,43 +38,46 @@ function insertInDocumentContent([word, count], index) {
 }
 
 async function parseHandler(e) {
-	minRepeats = document.getElementById("minimum-repeats").value.trim();
-	searchWord = document.getElementById("search-by-word").value.trim();
+	minRepeats = document.getElementById("min-repeats__input").value.trim();
+	searchWord = document.getElementById("search-word__input").value.trim();
 	olElement.textContent = "";
-	let tabId = activeTab.id;
-	let message = { minRepeats, searchWord };
-	let response = await tabsSendMessage(tabId, message);
-	let wordFrequency = JSON.parse(response);
+	const message = { minRepeats, searchWord };
+	const response = await tabsSendMessage(activeTab.id, message);
+	const wordFrequency = await excludeSelectedWords(JSON.parse(response));
 	wordFrequency.sort((a, b) => b[1] - a[1]);
 	wordFrequency.forEach(insertInDocumentContent);
 }
 
+async function excludeSelectedWords(words) {
+	const secondDomain = parseUrlDomain(activeTab.url, "second");
+	const { selectedWords } = await readFromStorage(secondDomain);
+	return words.filter(([word]) => !selectedWords.includes(word));
+}
+
 async function selectedWordHandler(e) {
-	const date = new Date();
-	let liElement = e.target.parentNode;
-	let [word, _, count] = liElement.innerText.split(" ");
-	let checkboxElement = e.target;
-	let description = {
-		addedDate: date,
-		numRepetitions: parseInt(count),
-	};
-	let data = await readFromStorage(word);
-	if (!data) {
-		await saveToStorage({ [word]: description });
+	const liElement = e.target.parentNode;
+	const [word] = liElement.innerText.split(" ");
+	const secondDomain = parseUrlDomain(activeTab.url, "second");
+	const storageData = await readFromStorage(secondDomain);
+	if (!storageData.selectedWords.includes(word)) {
+		await addSelectedWordToStorage(word, storageData, secondDomain);
 		liElement.remove();
-	} else {
-		checkboxElement.removeAttribute("type");
-		liElement.textContent += " (ALREDY STORED)";
-		liElement.classList.add("alredy-stored");
 	}
 }
 
-document
-	.getElementById("parse-button")
-	.addEventListener("click", parseHandler);
-document
-	.getElementById("search-by-word")
-	.addEventListener("input", parseHandler);
-document
-	.getElementById("minimum-repeats")
-	.addEventListener("input", parseHandler);
+async function addSelectedWordToStorage(word, data, secondDomain) {
+	const options = {
+		[secondDomain]: {
+			createdAt: data.createdAt,
+			selectedWords: [...data.selectedWords, word],
+			wordFrequency: [...data.wordFrequency],
+			paths: [...data.paths],
+			thirdDomains: [...data.thirdDomains],
+		},
+	};
+	await saveToStorage(options);
+}
+
+document.querySelector(".parse-button").addEventListener("click", parseHandler);
+document.getElementById("search-word__input").addEventListener("input", parseHandler);
+document.getElementById("min-repeats__input").addEventListener("input", parseHandler);
