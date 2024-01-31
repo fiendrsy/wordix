638d390364fd75c36f17a7e99b6bc81502d4901b
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-01-31 17:15:31 +0200

    added constants folder/file

---
+++ constants/constants.js
@@ -0,0 +1,20 @@
+export const DomainLevels = {
+  TOP: "top",
+  SECOND: "second",
+  THIRD: "third",
+};
+
+export const Domains = {
+  TWITCH: "twitch",
+  YOUTUBE: "youtube",
+  MONKEYTYPE: "monkeytype",
+  GITHUB: "github",
+};
+
+export const ErrorMessages = {
+  SAVE_STORAGE: "Save to storage an error!",
+  READ_ALL_STORAGE: "Read all from storage an error!",
+  READ_STORAGE: "Read from storage an error!",
+  ACTIVE_TAB: "Get active tab an error!",
+  SEND_TAB: "Tabs send message an error!"
+};
