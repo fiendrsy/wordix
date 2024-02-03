5a98cb815382440e0154371d9a4261dfb7ba5bde
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-03 13:05:28 +0200

    replaced from func. dec. to func. expr. & added try&catch statement

--- background/background.js
+++ background/background.js
@@ -1,102 +1,81 @@
 "use strict";
 
 import { Domains } from "../constants/constants.js";
-import * as date from "../helpers/date.js";
-import * as url from "../helpers/url.js";
 import * as tabs from "../helpers/tabs.js";
+import * as url from "../helpers/url.js";
 import * as storage from "../helpers/storage.js";
 
 const BAD_DOMAINS = [Domains.TWITCH, Domains.YOUTUBE, Domains.MONKEYTYPE, Domains.GITHUB];
 
-const _findMathedWords = (wordFrequency, cachedWords, isPathExist) =>
+const findMathedWords = (wordFrequency, cachedWords, isPathExist) =>
   !isPathExist
     ? wordFrequency
         .filter(([w]) => cachedWords.includes(w))
-        .reduce((acc, [w, f]) => {
-          acc[w] = f;
-          return acc;
-        }, {})
+        .reduce((acc, [w, f]) => ((acc[w] = f), acc), {})
     : {};
 
-const _findMissingWords = (wordFrequency, cachedWords) =>
+const findMissingWords = (wordFrequency, cachedWords) =>
   wordFrequency.filter(([w]) => !cachedWords.includes(w));
 
-function _mergeMissing(missingWords, matchedWords, cachedWordFrequency) {
-  const result = [];
+const mergeMissing = (missingWords, matchedWords, cachedWordFrequency) =>
+  cachedWordFrequency
+    .map(([word, frequency]) => [word, frequency + (matchedWords[word] || 0)])
+    .concat(missingWords);
 
-  for (let [word, frequency] of cachedWordFrequency) {
-    if (word in matchedWords) frequency += matchedWords[word];
-    result.push([word, frequency]);
+const processing = async (composedParts, data, wordFrequency) => {
+  try {
+    const isPathExist = data.paths.includes(composedParts.path);
+    const cachedWords = data.wordFrequency.map(([w]) => w);
+    const missingWords = findMissingWords(wordFrequency, cachedWords);
+    const matchedWords = findMathedWords(wordFrequency, cachedWords, isPathExist);
+    const missingWordsExist = !!missingWords.length;
+    const matchedWordsExist = !!Object.keys(matchedWords).length;
+
+    if (!missingWordsExist && !matchedWordsExist) return;
+
+    const options = storage.createOptions(
+      mergeMissing(missingWords, matchedWords, data.wordFrequency),
+      data,
+      composedParts,
+    );
+
+    await storage.save(options, composedParts.secondDomain);
+  } catch (ex) {
+    console.error(ex);
   }
+};
 
-  result.push(...missingWords);
+const observer = async (tabID, composedParts) => {
+  try {
+    const response = await tabs.sendMessage(tabID, {});
+    const wordFrequency = JSON.parse(response);
+    const data = await storage.read(composedParts.secondDomain);
 
-  return result;
-}
+    if (!data) {
+      const options = storage.createOptions(wordFrequency, null, composedParts);
+      await storage.save(options, composedParts.secondDomain);
+      return;
+    }
 
-async function tabsUpdateHandler(idUpdatedTab, changeInfo) {
-  const activeTab = await tabs.getActive();
-  const composedParts = url.composeParts(activeTab.url);
-
-  if (
-    changeInfo.status !== "complete" ||
-    activeTab.id !== idUpdatedTab ||
-    BAD_DOMAINS.includes(composedParts.secondDomain)
-  ) {
-    return;
+    processing(composedParts, data, wordFrequency);
+  } catch (ex) {
+    console.error(ex);
   }
+};
 
-  await observer(activeTab.id, composedParts);
-}
+const onUpdateTab = async (tabID, { status }, tab) => {
+  try {
+    if (status !== "complete") return;
 
-async function observer(tabId, composedParts) {
-  const response = await tabs.sendMessage(tabId, {});
-  const wordFrequency = JSON.parse(response);
-  const data = await storage.read(composedParts.secondDomain);
+    const composedParts = url.composeParts(tab.url);
+    const partsExist = !!Object.keys(composedParts).length;
 
-  if (!data) {
-    addNewContent(wordFrequency, composedParts);
-    return;
+    if (!partsExist || BAD_DOMAINS.includes(composedParts.secondDomain)) return;
+
+    await observer(tabID, composedParts);
+  } catch (ex) {
+    console.error(ex);
   }
+};
 
-  processing(composedParts, data, wordFrequency);
-}
-
-async function addNewContent(wordFrequency, { secondDomain, path, thirdDomain }) {
-  const options = {
-    createdAt: date.getCurrent(),
-    selectedWords: [],
-    wordFrequency,
-    paths: [path],
-    thirdDomains: [thirdDomain],
-  };
-
-  await storage.save(options, secondDomain);
-}
-
-async function updateContent(wordFrequency, data, { secondDomain, path, thirdDomain }) {
-  const options = {
-    createdAt: data.createdAt,
-    selectedWords: [...data.selectedWords],
-    wordFrequency,
-    paths: [...new Set([...data.paths, path])],
-    thirdDomains: [...new Set([...data.thirdDomains, thirdDomain])],
-  };
-
-  await storage.save(options, secondDomain);
-}
-
-async function processing(composedParts, data, wordFrequency) {
-  let cachedWords, isPathExist, missingWords, matchedWords;
-
-  isPathExist = data.paths.includes(composedParts.path);
-  cachedWords = data.wordFrequency.map(([w]) => w);
-  missingWords = _findMissingWords(wordFrequency, cachedWords);
-  matchedWords = _findMathedWords(wordFrequency, cachedWords, isPathExist);
-
-  if (missingWords.length === 0 && Object.values(matchedWords).length === 0) return;
-
-  updateContent(_mergeMissing(missingWords, matchedWords, data.wordFrequency), data, composedParts);
-}
-
-browser.tabs.onUpdated.addListener(tabsUpdateHandler);
+browser.tabs.onUpdated.addListener(onUpdateTab);
