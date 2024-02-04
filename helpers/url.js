7384bc56bd04eae79c78815ff33e6c2f359622a9
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-04 13:58:15 +0200

    added check of system url

--- helpers/url.js
+++ helpers/url.js
@@ -1,24 +1,17 @@
-export const DomainLevels = {
-  TOP: "top",
-  SECOND: "second",
-  THIRD: "third",
-};
+import { DomainLevels } from "../constants/constants.js";
 
-export function extractDomain(url, domainLevel) {
-  if (!URL.canParse(url)) return "";
-  const hostname = new URL(url).hostname;
-  if (!domainLevel) return hostname;
-  const domains = hostname.split(".").map(_normalizeDomain);
-  return _selectDomain(domains, domainLevel);
-}
+const normalizeDomain = (domain) =>
+  typeof domain !== "string" ? domain : domain.trim().toLowerCase();
 
-function _normalizeDomain(domain) {
-  if (typeof domain !== "string") return domain;
-  return domain.toLowerCase().trim();
-}
+const isSystemURL = (url) =>
+  url.startsWith("about:") ||
+  url.startsWith("chrome:") ||
+  url.startsWith("local:") ||
+  url.startsWith("view-source:");
 
-function _selectDomain(domains, domainLevel) {
+function selectDomain(domains, domainLevel) {
   const thirdDomain = domains.length > 2 ? domains.shift() : "";
+
   switch (domainLevel) {
     case DomainLevels.TOP:
       return domains[1];
@@ -29,18 +22,34 @@ function _selectDomain(domains, domainLevel) {
   }
 }
 
-export function compose(url) {
+const extractPath = (url) => new URL(url).pathname;
+
+function extractDomain(url, domainLevel) {
+  const hostname = new URL(url).hostname;
+
+  if (!domainLevel) return hostname;
+
+  const domains = hostname.split(".").map(normalizeDomain);
+
+  return selectDomain(domains, domainLevel);
+}
+
+export function isValidURL(url) {
+  return URL.canParse(url) && !isSystemURL(url);
+}
+
+export function composeParts(url) {
+  if (!isValidURL(url)) return {};
+
   const thirdDomain = extractDomain(url, DomainLevels.THIRD);
-  const path = extractPath(url);
   const secondDomain = extractDomain(url, DomainLevels.SECOND);
+  const topDomain = extractDomain(url, DomainLevels.TOP);
+  const path = extractPath(url);
+
   return {
     thirdDomain,
     secondDomain,
+    topDomain,
     path,
   };
-}
-
-export function extractPath(url) {
-  if (!URL.canParse(url)) return "";
-  return new URL(url).pathname;
 }
