73b644d9236c837869d7257815cced918e1598f3
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-09 14:33:04 +0200

    impl. handling wrong args. inside createOption func. & added strict mode

--- helpers/storage.js
+++ helpers/storage.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import { ErrorMessages } from "../constants/constants.js";
 import { logger } from "./logger.js";
 
@@ -18,21 +20,15 @@ const prepareValue = (value) => {
 export async function readAll() {
   try {
     const data = await browser.storage.local.get();
-    const isError = browser.runtime.lastError;
-
-    if (isError) {
-      logger(readAll.name, FILE_NAME, { arguments, data });
-      console.error(ErrorMessages.BROWSER_ERROR, isError);
-
-      return {};
-    }
 
     if (!data)
       return {};
     else
       return data;
   } catch (ex) {
-    logger(readAll.name, FILE_NAME, arguments);
+    const isError = browser.runtime.lastError;
+
+    logger(readAll.name, FILE_NAME, { arguments, isError });
     console.error(ErrorMessages.READ_ALL_STORAGE, ex);
 
     return {};
@@ -43,24 +39,20 @@ export async function read(key) {
   if (!key) {
     logger(read.name, FILE_NAME, arguments);
     console.error(`${key} ${ErrorMessages.WRONG_VALUE}`);
+
     return {};
   }
   try {
     const data = await browser.storage.local.get([key]);
-    const isError = browser.runtime.lastError;
-
-    if (isError) {
-      logger(read.name, FILE_NAME, { arguments, data });
-      console.error(ErrorMessages.BROWSER_ERROR, isError);
-      return {};
-    }
 
     if (!data)
       return {};
     else
       return data[key];
   } catch (ex) {
-    logger(read.name, FILE_NAME, arguments);
+    const isError = browser.runtime.lastError;
+
+    logger(read.name, FILE_NAME, { arguments, isError });
     console.error(ErrorMessages.READ_STORAGE, ex);
 
     return {};
@@ -70,31 +62,55 @@ export async function read(key) {
 export async function save(data, key) {
   if (!data || !key) {
     logger(save.name, FILE_NAME, arguments);
-    console.error(`${data} ${ErrorMessages.WRONG_VALUE} ${key} ${ErrorMessages.WRONG_VALUE}`);
+    console.error(
+      `${data} ${ErrorMessages.WRONG_VALUE}
+       ${key} ${ErrorMessages.WRONG_VALUE}`
+    );
   }
   try {
     const options = {};
     options[key] = data;
 
+    logger(save.name, FILE_NAME, { arguments, options });
+
     await browser.storage.local.set(options);
+
+  } catch (ex) {
     const isError = browser.runtime.lastError;
 
-    if (isError) {
-      logger(save.name, FILE_NAME, { arguments, options });
-      console.error(ErrorMessages.BROWSER_ERROR, isError);
-    }
-  } catch (ex) {
-    logger(save.name, FILE_NAME, arguments);
+    logger(save.name, FILE_NAME, { arguments, isError });
     console.error(ErrorMessages.SAVE_STORAGE, ex);
   }
 }
 
-export function createOptions(wordFrequency, data, { path, thirdDomain }) {
+export function createOptions(wordFrequency, data, partsURL) {
+  if (!wordFrequency || !partsURL) {
+    logger(createOptions.name, FILE_NAME, { arguments });
+    console.error(
+     `${wordFrequency} ${ErrorMessages.WRONG_VALUE}
+      ${partsURL} ${ErrorMessages.WRONG_VALUE}`
+    );
+
+    return {};
+  }
   try {
-    const createdAt = data ? data.createdAt : new Date();
-    const selectedWords = data ? [...data.selectedWords] : [];
-    const paths = data ? [...data.paths, path] : [path];
-    const thirdDomains = data ? [...data.thirdDomains, thirdDomain] : [thirdDomain];
+    const { path, thirdDomain } = partsURL;
+
+    const createdAt = data
+      ? data.createdAt
+      : new Date();
+
+    const selectedWords = data
+      ? [...data.selectedWords]
+      : [];
+
+    const paths = data
+      ? [...data.paths, path]
+      : [path];
+
+    const thirdDomains = data
+      ? [...data.thirdDomains, thirdDomain]
+      : [thirdDomain];
 
     const options = {
       createdAt,
@@ -112,7 +128,7 @@ export function createOptions(wordFrequency, data, { path, thirdDomain }) {
 
     return options;
   } catch (ex) {
-    logger(createOptions.name, FILE_NAME, options);
+    logger(createOptions.name, FILE_NAME, arguments);
     console.error(ex);
   }
 }
