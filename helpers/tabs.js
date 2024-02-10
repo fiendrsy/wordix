0c22d14a1b3d5cba19f4d36b1ef468b2d7ba4067
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-10 17:32:41 +0200

    impl. error handling, added strict mode

--- helpers/tabs.js
+++ helpers/tabs.js
@@ -1,20 +1,49 @@
+"use strict";
+
 import { ErrorMessages } from "../constants/constants.js";
+import { logger } from "./logger.js";
 
-export function sendMessage(tabID, message) {
-  try {
-    let response = browser.tabs.sendMessage(tabID, message);
-    return response;
-  } catch (ex) {
-    throw new Error(ErrorMessages.SEND_TAB, ex);
-  }
-}
+// The current file name needed for logger
+const FILE_NAME = "tabs.js";
 
-export async function getActive() {
-  try {
-    const queryOptions = { active: true, currentWindow: true };
-    const [data] = await browser.tabs.query(queryOptions);
-    return data;
-  } catch (ex) {
+export const sendMessage = async function (tabID, message = {}) {
+  if (!tabID) {
+    logger(sendMessage.name, FILE_NAME, { arguments });
+    console.error(`${tabID} ${ErrorMessages.WRONG_VALUE}`);
+
     throw new Error(ErrorMessages.ACTIVE_TAB, ex);
   }
-}
+  try {
+    const response = browser.tabs.sendMessage(tabID, message);
+
+    logger(sendMessage.name, FILE_NAME, { arguments, response });
+
+    return response;
+  } catch (ex) {
+    const isError = browser.runtime.lastError;
+
+    logger(sendMessage.name, FILE_NAME, { arguments, isError });
+    console.error(ErrorMessages.SEND_TAB, ex);
+
+    throw new Error(ErrorMessages.ACTIVE_TAB, ex);
+  }
+};
+
+export const getActive = async function () {
+  try {
+    const queryOptions = { active: true, currentWindow: true };
+
+    logger(getActive.name, FILE_NAME, { arguments, queryOptions });
+
+    const data = await browser.tabs.query(queryOptions);
+
+    return data.pop();
+  } catch (ex) {
+    const isError = browser.runtime.lastError;
+
+    logger(sendMessage.name, FILE_NAME, { arguments, isError });
+    console.error(ErrorMessages.ACTIVE_TAB, ex);
+
+    throw new Error(ErrorMessages.ACTIVE_TAB, ex);
+  }
+};
