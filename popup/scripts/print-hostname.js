ad84a4325569dd1ce25cd5e5c074a0c0378df084
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-13 11:19:19 +0200

    added a script that prints the hostname of the current website

---
+++ popup/scripts/print-hostname.js
@@ -0,0 +1,19 @@
+import * as dom from "./dom.js";
+import { logger } from "../../helpers/logger.js";
+import { ErrorMessages } from "../../constants/constants.js";
+
+// The current file name needed for logger
+const FILE_NAME = "print-hostnama.js";
+
+export const printHostname = (hostname) => {
+  if (typeof hostname !== "string") {
+    logger(printHostname.name, FILE_NAME, { hostname });
+    console.error(`${hostname} ${ErrorMessages.WRONG_VALUE}`);
+
+    return;
+  }
+
+  dom.reWrite(".current-site-address", hostname);
+
+  void 0;
+};
