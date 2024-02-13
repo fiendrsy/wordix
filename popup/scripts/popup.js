f29c474d2fdeeef9462598626d5fba6e85173136
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-13 17:57:10 +0200

    added popup script

---
+++ popup/scripts/popup.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import * as dom from "./dom.js";
+import { onParse } from "./parse.js";
+import { onUpload } from "./session.js";
+import { printHostname } from "./print-hostname.js";
+import { logger } from "../../helpers/logger.js";
+import * as tabs from "../../helpers/tabs.js";
+import * as url from "../../helpers/url.js";
+import { writeErrors } from "../../helpers/error.js";
+
+// The current file name needed for logger
+const FILE_NAME = "popup.js";
+
+// The function is fire when DOM is loaded (means extension dom)
+const popup = async function () {
+  try {
+    const tab = await tabs.getActive();
+    const partsURL = url.composeParts(tab.url);
+
+    logger(popup.name, FILE_NAME, { arguments, partsURL });
+
+    if (!partsURL) return;
+
+    printHostname(partsURL.hostname);
+
+    dom.addLis(".parse-button", "click", onParse, tab, partsURL);
+    dom.addLis("#search-word__input", "input", onParse, tab, partsURL);
+    dom.addLis("#min-repeats__input", "input", onParse, tab, partsURL);
+    dom.addLis(".upload-button", "click", onUpload);
+
+    void 0;
+  } catch (ex) {
+    logger(popup.name, FILE_NAME, arguments);
+    writeErrors(ex);
+  }
+};
+
+dom.addLis(document, "DOMContentLoaded", popup);
