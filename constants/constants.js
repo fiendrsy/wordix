a6ebfbb87ba19526ae3408da006b74cffd57481f
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-02-18 14:03:45 +0200

    removed unused constants

--- constants/constants.js
+++ constants/constants.js
@@ -12,18 +12,14 @@ export const BlackListDomains = {
 };
 
 export const ErrorMessages = {
-  SAVE_STORAGE: "Save to storage an error!",
-  READ_ALL_STORAGE: "Read all from storage an error!",
-  READ_STORAGE: "Read from storage an error!",
-  ACTIVE_TAB: "Get active tab an error!",
-  SEND_TAB: "Tabs send message an error!",
-  WRONG_VALUE: "<- Wrong value!",
-  INIT_PROBLEM: "Script is not initiated an error!",
+  STACK: "Stack:",
+  DETAILS: "Error details:",
+  WRONG_VALUE: "<- Wrong value",
 };
 
 export const FragmentsURL = {
-  HTTP: "http://",
-  HTTPS: "https://",
+  HTTP: "http",
+  HTTPS: "https",
   LOCALHOST: "localhost",
   NUMERIC_HOST: "127.0.0.1",
 };
