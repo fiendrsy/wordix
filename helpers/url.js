7b6e013ef17b05109e2adf1044003e78c7bbb2d3
fiendrsy <111488436+fiendrsy@users.noreply.github.com>
2024-03-02 18:16:24 +0200

    added processing of url parameters

--- helpers/url.js
+++ helpers/url.js
@@ -15,19 +15,20 @@ const selectDomain = function (domains, domainLevel) {
   }
 };
 
-const normalizeDomain = (domain) => String(domain).toLowerCase().trim();
+const normalizeDomain = (domain) => domain.toString().toLowerCase().trim();
 
 const extractDomain = function (url, domainLevel) {
   const hostname = new URL(url).hostname;
 
-  if (!domainLevel) return hostname;
+  if (!domainLevel)
+    return hostname;
 
   const domains = hostname.split(".").map(normalizeDomain);
 
   return selectDomain(domains, domainLevel);
 };
 
-const extractPath = (url) => new URL(url).pathname;
+const extractPath = (url) => new URL(url).pathname + new URL(url).search;
 
 const isLocalHost = (url) =>
   extractDomain(url) === FragmentsURL.LOCALHOST || extractDomain(url) === FragmentsURL.NUMERIC_HOST;
@@ -37,7 +38,8 @@ const isHTTP = (url) => url.startsWith(FragmentsURL.HTTP) || url.startsWith(Frag
 const isValidURL = (url) => URL.canParse(url) && isHTTP(url) && !isLocalHost(url);
 
 export const composeParts = function (url) {
-  if (!isValidURL(url)) return null;
+  if (!isValidURL(url))
+    return null;
 
   const hostname = extractDomain(url);
   const thirdDomain = extractDomain(url, DomainLevels.THIRD);
